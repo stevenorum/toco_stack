c8c488c0faa571ede2fcebb0744998a30e47803f
stevenorum <stevenorum@gmail.com>
2016-02-04 16:08:40 -0500

    Adding the basics of the password reset page.

--- toco_stack/views.py
+++ toco_stack/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import RequestContext, loader
 import logging
-from toco.user import User, SessionToken
+from toco.user import *
 
 logger = logging.getLogger(__name__)
 
@@ -22,23 +22,47 @@ def index(request, message=None):
     print("Index method ending.")
     return response
 
+def password_reset_request(request, request_id = None):
+    print("password_reset_request method beginning.")
+    response = render(request, 'index.html', {'password_reset_request_id':request_id})
+    print("password_reset_request method ending.")
+    return response
+
+# ADD PAGE TO WHICH CUSTOMERS ARE DIRECTED WHEN FOLLOWING A PASSWORD RESET REQUEST LINK
+
+def request_password_reset(request):
+    # FINISH IMPLEMENTING
+    print("request_password_reset method beginning.")        
+    email = request.POST.get('email')
+    reset_request = PasswordResetRequest(email=email)
+    reset_request.create()
+    reset_link = '/'.join(request.build_absolute_uri().split('/')[:-1])+'/password_reset_request/'+str(reset_request.id)
+    print(reset_link)
+    # SAFETY CHECK FOR DEVELOPMENT - REMOVE LATER
+    valid_email_parts = ['stevenorum','toco']
+    if [x for x in valid_email_parts if x in email]:
+        print(send_mail('toco subject', 'Go to {} to reset your password.'.format(reset_link), settings.DEFAULT_FROM_EMAIL,
+                        [email], fail_silently=False))
+    print("request_password_reset method ending.")
+    return response
+
 def reset_password(request):
     print("reset password method beginning.")        
     response = HttpResponseRedirect(request.POST.get('from', '/'))
-    email = request.POST.get('email')
-
-    print(settings.EMAIL_HOST_USER)
-    print(send_mail('toco subject', 'This is an example toco message.', settings.DEFAULT_FROM_EMAIL,
-              [email], fail_silently=False))
-
-#     response = HttpResponseRedirect(request.POST.get('from', '/'))
-#     if email and password:
-#         user = User.load_with_auth(email, password)
-#         if user:
-#             token = user.get_new_session_token(HTTP_USER_AGENT=request.META.get("HTTP_USER_AGENT"), REMOTE_ADDR=request.META.get("REMOTE_ADDR"))
-#             response.set_cookie(SessionToken.CKEY, value=token.id, expires=token.expiry_datetime, secure=None, httponly=True)
-#             request.user=user
-#             request.session=token
+    if request.user:
+        if not request.user.password_is_correct(request.POST.get('current_password')):
+            # ADD ERROR HERE
+            return rsponse
+    else:
+        user = User.load_from_password_reset_request(request.POST.get('reset_request_id'))
+        if not user:
+            # ADD ERROR HERE
+            return response
+        request.user = user
+    new_password_1 = request.POST.get('password')
+    new_password_2 = request.POST.get('confirm_password')
+    if new_password_1 == new_password_2:
+        request.user.set_password(new_password_1)
     print("reset password method ending.")
     return response
 
